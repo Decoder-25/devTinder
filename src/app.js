212b17e29e0ea9f5b6d7536e6b0d539833d76d6a
Disha Biswas <codeblossom@Dishas-MacBook-Air-4.local>
2025-02-16 03:42:41 +0000

    Experimented with validation in signup and login

--- src/app.js
+++ src/app.js
@@ -2,43 +2,117 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database.js");
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
+const { validateSignupData } = require("./utils/validation.js");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth.js");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
     try {
+        // Validation of the data
+
+        validateSignupData(req);
+
+        // Encrypt the password
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        // Creating a new instance for the user model
+
+        const user = new User({
+            firstName, lastName, emailId, password: passwordHash,
+        });
+
         await user.save();
         res.send("User created successfully.");
+
     } catch (error) {
-        res.status(400).send("Error saving the user:" + error.message);
-    }  
+        res.status(400).send("ERROR : " + error.message);
+    }
+});
+
+app.post("/login", async (req, res) => {
+
+    try {
+        const { emailId, password } = req.body;
+
+
+        const user = await User.findOne({ emailId: emailId });
+
+        if (!user) {
+            throw new Error("Invalid credentials!");
+
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (isPasswordValid) {
+
+            // Create a JWT token 
+
+            const token = await jwt.sign({ _id: user._id }, "DISHA@Tinder$678");
+
+            //Add the token to cookie and send the response back to user
+            res.cookie("token", token);
+            res.send("Login successful!")
+        }
+        else {
+            throw new Error("Invalid credentials!");
+        }
+
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+    }
+
+
+
+});
+
+app.get("/profile", userAuth, async (req, res) => {
+
+    try {
+
+        const user = req.user;
+
+
+        res.send(user);
+    }
+    catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+    }
+
 });
 
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
     try {
-        const user = await User.find({emailId: userEmail});
-        if(user.length == 0){
+        const user = await User.find({ emailId: userEmail });
+        if (user.length == 0) {
             res.status(404).send("User not found");
         }
         else {
             res.send(user);
         }
-       
+
     } catch (error) {
         res.status(400).send("Something went wrong");
     }
 });
 
-app.get("/feed", async (req,res) => {
-  try {
-    const user = await User.find({});
-    res.send(user);
-  } catch (error) {
-    res.status(404).send("Something went wrong");
-  }
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.send(user);
+    } catch (error) {
+        res.status(404).send("Something went wrong");
+    }
 });
 
 app.patch("/user", async (req, res) => {
@@ -46,7 +120,7 @@ app.patch("/user", async (req, res) => {
     const data = req.body;
 
     try {
-        await User.findByIdAndUpdate({ _id: userId}, data);
+        await User.findByIdAndUpdate({ _id: userId }, data);
         res.send("user updated successfully");
     } catch (error) {
         res.status(400).send("something went wrong");
@@ -57,7 +131,7 @@ connectDB()
     .then(() => {
         console.log("Database connection established....");
         const PORT = process.env.PORT || 7000;
-        app.listen(PORT,() => {
+        app.listen(PORT, () => {
             console.log(`Server is sucessfully running on port ${PORT}....`);
         });
     })
